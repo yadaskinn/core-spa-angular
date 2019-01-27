e7efb30991c2d80ee4880a288df575937f4c9571
yadaskinn <yadaskinn@gmail.com>
2019-01-27 16:09:30 +0300

    repository oluşturuldu

---
+++ SehirRehberi/API/SehirRehberi.API/Data/IAppRepository.cs
@@ -0,0 +1,17 @@
+﻿using SehirRehberi.API.Models;
+using System.Collections.Generic;
+
+namespace SehirRehberi.API.Data
+{
+    public interface IAppRepository
+    {
+        void Add<T>(T entity) where T : class;
+        void Delete<T>(T entity) where T : class;
+        bool SaveAll();
+
+        List<City> GetCities();
+        List<Photo> GetPhotosByCity(int cityId);
+        City GetCityById(int cityId);
+        Photo GetPhoto(int id);
+    }
+}
--- SehirRehberi/API/SehirRehberi.API/Startup.cs
+++ SehirRehberi/API/SehirRehberi.API/Startup.cs
@@ -29,6 +29,7 @@ namespace SehirRehberi.API
             services.AddDbContext<DataContext>(x => x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc();
             services.AddCors();
+            services.AddScoped<IAppRepository, AppRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
